2973ea7763c936636302330f602cafd77259fcac
lc3lx <lc3lx@users.noreply.github.com>
2026-08-26 08:53:20 +0300

    fsih

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 
+declare module '*.mpeg' {
+  const src: string;
+  export default src;
+}
+
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL?: string;
   readonly VITE_DEV_PROXY_TARGET?: string;
